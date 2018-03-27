c21e80c315a7d7b39fd45f9d41ad1b662d5d8ac7
Jacob Berman <jacobberman1995@gmail.com>
2018-03-27 18:18:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var newString = new Array();
-  for (let i = 0; i <musicians.length -1; i++){
+  for (let i = 0; i <musicians.length; i++){
    newString[i] = '${i}'
    // sentances[i] = '${musicians[i]} plays $[instruments[i]]'
   }
